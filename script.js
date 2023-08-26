c99ccaf719848570de9e79b4faea1e4c6e82c603
Bryan Chow <bchow.post@gmail.com>
2023-08-26 19:47:56 +0100

    Final

--- script.js
+++ script.js
@@ -27,17 +27,11 @@ const buildTree = (arr, start = 0, end = arr.length - 1) => {
 };
 
 const treeFactory = (arr) => {
-	newArr = [...new Set(arr)].sort();
+	newArr = [...new Set(arr)].sort(function (a, b) {
+		return a - b;
+	});
 
 	let root = buildTree(newArr);
-
-	const inOrder = (node = root) => {
-		if (node !== null) {
-			inOrder(node.left);
-			console.log(node.value);
-			inOrder(node.right);
-		}
-	};
 
 	const insertNode = (value, node = root) => {
 		if (node == null) {
@@ -104,22 +98,187 @@ const treeFactory = (arr) => {
 	};
 
 	const levelOrder = (func, node = root) => {
-		travelOrder = [node];
-		if (func == null) {
-			arr = [];
-			func = arr.push[node];
+		travelQueue = [node];
+		const travelFunc = () => {
+			while (travelQueue.length != 0) {
+				if (travelQueue[0].left != null) {
+					travelQueue.push(travelQueue[0].left);
+				}
+				if (travelQueue[0].right != null) {
+					travelQueue.push(travelQueue[0].right);
+				}
+				func(travelQueue.shift().value);
+			}
+		};
+		if (func == undefined) {
+			let arr = [];
+			func = (a) => {
+				arr.push(a);
+				return arr;
+			};
+			travelFunc();
+			return arr;
+		} else {
+			return travelFunc();
 		}
 	};
 
-	return { root, insertNode, deleteNode, findNode };
+	const preOrder = (func, node = root) => {
+		const preOrderRecurse = () => {
+			if (node == null) {
+				return;
+			} else {
+				func(node.value);
+				preOrder(func, node.left);
+				preOrder(func, node.right);
+			}
+		};
+		if (func == undefined) {
+			let arr = [];
+			func = (a) => {
+				arr.push(a);
+				return arr;
+			};
+			preOrderRecurse();
+			return arr;
+		} else {
+			return preOrderRecurse();
+		}
+	};
+
+	const inOrder = (func, node = root) => {
+		const inOrderRecurse = () => {
+			if (node == null) {
+				return;
+			} else {
+				inOrder(func, node.left);
+				func(node.value);
+				inOrder(func, node.right);
+			}
+		};
+		if (func == undefined) {
+			let arr = [];
+			func = (a) => {
+				arr.push(a);
+				return arr;
+			};
+			inOrderRecurse();
+			return arr;
+		} else {
+			return inOrderRecurse();
+		}
+	};
+
+	const postOrder = (func, node = root) => {
+		const postOrderRecurse = () => {
+			if (node == null) {
+				return;
+			} else {
+				postOrder(func, node.left);
+				postOrder(func, node.right);
+				func(node.value);
+			}
+		};
+		if (func == undefined) {
+			let arr = [];
+			func = (a) => {
+				arr.push(a);
+				return arr;
+			};
+			postOrderRecurse();
+			return arr;
+		} else {
+			return postOrderRecurse();
+		}
+	};
+
+	const height = (value) => {
+		const findHeight = (node) => {
+			if (node == null) {
+				return -1;
+			} else {
+				let lHeight = findHeight(node.left);
+				let rHeight = findHeight(node.right);
+				if (lHeight > rHeight) {
+					return lHeight + 1;
+				} else {
+					return rHeight + 1;
+				}
+			}
+		};
+		return findHeight(findNode(value));
+	};
+
+	const depth = (value, node = root) => {
+		if (node == null) {
+			return -1;
+		}
+		let totalDepth = -1;
+		if (
+			node.value == value ||
+			(totalDepth = depth(value, node.left)) >= 0 ||
+			(totalDepth = depth(value, node.right)) >= 0
+		) {
+			return totalDepth + 1;
+		}
+	};
+
+	const isBalanced = (node = root) => {
+		let lSubDepth = height(node.left.value);
+		let rSubDepth = height(node.right.value);
+		if (Math.abs(lSubDepth - rSubDepth) <= 1) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	const rebalance = () => {
+		let arr = preOrder().sort(function (a, b) {
+			return a - b;
+		});
+		root = treeFactory(arr);
+		return root;
+	};
+
+	return {
+		newArr,
+		root,
+		insertNode,
+		deleteNode,
+		findNode,
+		levelOrder,
+		preOrder,
+		inOrder,
+		postOrder,
+		height,
+		depth,
+		isBalanced,
+		rebalance,
+	};
 };
 
-arr = [2, 1, 3, 4, 6, 5, 7];
-let t = treeFactory(arr);
-t.insertNode(-1);
-t.insertNode(-2);
-t.insertNode(8);
-t.insertNode(9);
-prettyPrint(t.findNode(-10));
+const driver = () => {
+	arr = Array.from({ length: 100 }, () => Math.floor(Math.random() * 100));
+	let t = treeFactory(arr);
+	console.log(t.isBalanced());
+	console.log(t.levelOrder());
+	console.log(t.preOrder());
+	console.log(t.inOrder());
+	console.log(t.postOrder());
+	t.insertNode(111);
+	t.insertNode(123);
+	t.insertNode(125);
+	t.insertNode(215);
+	t.insertNode(412);
+	t.insertNode(193);
+	console.log(t.isBalanced());
+	t = t.rebalance();
+	console.log(t.isBalanced());
+	console.log(t.levelOrder());
+	console.log(t.preOrder());
+	console.log(t.postOrder());
+	console.log(t.inOrder());
+};
 
-prettyPrint(t.root);
+driver();
